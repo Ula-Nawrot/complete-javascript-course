a0908c3c33cff12323b5277d90f1a58f53c8373d
UlaNawrot <ula.nawrot@gmail.com>
2020-12-22 21:08:51 +0100

    adding logic to the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 50;
+// console.log(document.querySelector('.guess').value = 23);
+const secretNumber = Math.trunc(Math.random()*20+1);
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (secretNumber === guess) {
+        document.querySelector('.message').textContent = 'Correct Number';
+    } else if (secretNumber < guess) {
+        if(score > 1){document.querySelector('.message').textContent = 'Too high';
+        score--;
+        document.querySelector('.score').textContent = score;
+        }else {
+            document.querySelector('.message').textContent = 'You lost the game';
+        }
+        
+    } else if (secretNumber > guess){
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too low';
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+        }
+        
+    }
+})
