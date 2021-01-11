80c38055172d93958fa9ee404b329713efc033f8
UlaNawrot <ula.nawrot@gmail.com>
2021-01-11 12:24:25 +0100

    changing Css style

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,16 +8,22 @@
 // console.log(document.querySelector('.guess').value = 23);
 const secretNumber = Math.trunc(Math.random()*20+1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
+    // Where there is no input
     if(!guess){
         document.querySelector('.message').textContent = 'No number!';
+        // when guess is correct/ when palyer wins
     } else if (secretNumber === guess) {
         document.querySelector('.message').textContent = 'Correct Number';
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+        //When guess is to high
     } else if (secretNumber < guess) {
         if(score > 1){document.querySelector('.message').textContent = 'Too high';
         score--;
@@ -25,7 +31,7 @@ document.querySelector('.check').addEventListener('click', function() {
         }else {
             document.querySelector('.message').textContent = 'You lost the game';
         }
-        
+        // When guess is to low
     } else if (secretNumber > guess){
         if(score > 1){
             document.querySelector('.message').textContent = 'Too low';
@@ -37,3 +43,19 @@ document.querySelector('.check').addEventListener('click', function() {
         
     }
 })
+
+const el = document.querySelector('.check');
+// Get the input field
+const input = document.querySelector('.guess');
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    el.click();
+  }
+});
+
