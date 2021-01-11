b8d42f465e86825b9ef4b588fde6e318ee6871fd
UlaNawrot <ula.nawrot@gmail.com>
2021-01-11 14:20:01 +0100

    adding functionality of highest score and refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -8,6 +8,11 @@
 // console.log(document.querySelector('.guess').value = 23);
 let secretNumber = Math.trunc(Math.random()*20+1);
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
 
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -15,34 +20,52 @@ document.querySelector('.check').addEventListener('click', function() {
     console.log(guess, typeof guess);
     // Where there is no input
     if(!guess){
-        document.querySelector('.message').textContent = 'No number!';
+        //document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!');
         // when guess is correct/ when palyer wins
     } else if (secretNumber === guess) {
-        document.querySelector('.message').textContent = 'Correct Number';
+        //document.querySelector('.message').textContent = 'Correct Number';
+        displayMessage('Correct Number');
         document.querySelector('.number').textContent = secretNumber;
 
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'
-        //When guess is to high
-    } else if (secretNumber < guess) {
-        if(score > 1){document.querySelector('.message').textContent = 'Too high';
+
+        if(score > highscore){
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        } 
+        //When guess is wrong
+    } else if(guess !== secretNumber){
+        if(score > 1){
+            //document.querySelector('.message').textContent = guess > secretNumber ? 'Too high' : 'Too low';
+            displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
         score--;
         document.querySelector('.score').textContent = score;
         }else {
-            document.querySelector('.message').textContent = 'You lost the game';
+            //document.querySelector('.message').textContent = 'You lost the game';
+            displayMessage('You lost the game');
         }
-        // When guess is to low
-    } else if (secretNumber > guess){
-        if(score > 1){
-            document.querySelector('.message').textContent = 'Too low';
-        score--;
-        document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game';
-        }
+    }})
+        //When guess is to high
+    // } else if (secretNumber < guess) {
+    //     if(score > 1){document.querySelector('.message').textContent = 'Too high';
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    //     }else {
+    //         document.querySelector('.message').textContent = 'You lost the game';
+    //     }
+    //     // When guess is to low
+    // } else if (secretNumber > guess){
+    //     if(score > 1){
+    //         document.querySelector('.message').textContent = 'Too low';
+    //     score--;
+    //     document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = 'You lost the game';
+    //     }
         
-    }
-})
+    
 
 const el = document.querySelector('.check');
 // Get the input field
@@ -65,7 +88,8 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent = 'Start guessing...';
+    //document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#0E2518';
     document.querySelector('.number').style.width = '15rem';
