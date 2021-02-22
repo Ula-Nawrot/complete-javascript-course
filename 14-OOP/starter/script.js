674c47eaacc31fc9035bc948b69b8512414e681f
UlaNawrot <ula.nawrot@gmail.com>
2021-02-22 12:16:42 +0100

    Challenge 2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,25 +1,55 @@
 'use strict';
 
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed
-};
+//Challenge 1
 
-Car.prototype.accelerate = function () {
-    const accelerate = this.speed + 10;
-    console.log(`New speed ${accelerate} km/h`);
-};
+// const Car = function (make, speed) {
+//     this.make = make;
+//     this.speed = speed
+// };
 
-Car.prototype.break = function () {
-    const breakCar = this.speed - 5;
-    console.log(`Speed after breaking ${breakCar} km/h`);
+// Car.prototype.accelerate = function () {
+//     const accelerate = this.speed + 10;
+//     console.log(`New speed ${accelerate} km/h`);
+// };
+
+// Car.prototype.break = function () {
+//     const breakCar = this.speed - 5;
+//     console.log(`Speed after breaking ${breakCar} km/h`);
+// }
+
+// const car1 = new Car ('BMW', 120);
+// const car2 = new Car ('Mercedes', 95);
+
+// car1.accelerate();
+// car1.accelerate();
+// car2.accelerate();
+// car1.break();
+// car2.break();
+
+//Challenge 2
+
+class Car {
+    constructor(make,speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    accelerate() {
+        return this.speed += 10;
+    }
+    break() {
+        return this.speed -= 5;
+    }
+    get speedUS() {
+        return this.speed/1.6
+    }
+    set speedUS(speed) {
+        return this.speed = speed * 1.6;
+    }
 }
 
-const car1 = new Car ('BMW', 120);
-const car2 = new Car ('Mercedes', 95);
-
-car1.accelerate();
-car1.accelerate();
-car2.accelerate();
-car1.break();
-car2.break();
+const car2 = new Car('BMW', 120);
+console.log(car2);
+console.log(car2.accelerate());
+console.log(car2.speedUS); 
+car2.speedUS = 80;
+console.log(car2);
